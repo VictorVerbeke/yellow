8de6270c610e458bbbb356a6a6e3d1c1c71b6edf
Dimitris Kokkonis <kokkonisd@gmail.com>
2019-01-14 09:49:28 +0100

    Testing window

---
+++ learning/testing.cc
@@ -0,0 +1,41 @@
+#include <SFML/Window.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // change the position of the window (relatively to the desktop)
+    window.setPosition(sf::Vector2i(10, 10));
+
+    // change the size of the window
+    window.setSize(sf::Vector2u(640, 480));
+
+    // change the title of the window
+    window.setTitle("SFML window");
+
+    // get the size of the window
+    sf::Vector2u size = window.getSize();
+    unsigned int width = size.x;
+    unsigned int height = size.y;
+
+    cout << width << endl;
+    cout << height << endl;
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    return 0;
+}
