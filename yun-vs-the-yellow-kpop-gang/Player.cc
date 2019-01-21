c3487b81077a6f5644c5a999af88fb809e939d42
3678549 <lisa.hennebelle@upmc.fr>
2019-01-21 17:22:59 +0100

    MDR ca marche pas help please

--- yun-vs-the-yellow-kpop-gang/Player.cc
+++ yun-vs-the-yellow-kpop-gang/Player.cc
@@ -5,7 +5,13 @@ using namespace std;
 Player::Player ():
     Character(0, 0, 100, 100, "images/yun_test_100.png"),
     _invulCD(0),
-    _fireCD(0){}
+    _fireCD(0)
+{
+    // Initialisation des attributs partagés par les characters. On suppose que
+    // la difficulté de base est à "Normal".
+    // Toutes ces valeurs sont exprimées en frames, soit n * 1/60 secondes.
+
+}
 
 Player::Player (float x, float y, float size, string imagePath)
 :
@@ -19,7 +25,7 @@ Player::~Player(){
 // Overloads
 void Player::operator-(const float &b) {
     this->setHp(this->getHp() - b);
-    this->_invulCD = INVULTEMPS;
+    this->_invulCD = _playerInvulCD;
     cout << "Yun a perdu " << b << " hp et est à " << this->getHp() << " hp." << endl;
 }
 
@@ -27,9 +33,9 @@ void Player::operator-(const float &b) {
 
 Pellet* Player::fire(){
     if (_fireCD == 0) {
-        _fireCD = PLAYERFIRECD;
+        _fireCD = _playerFireCD;
         Pellet *created = new Pellet(0, 0, 32, "images/angery_32.png",
-                                    PLAYERPELLETSPEED, 0, 1, 1); //90 = down
+                            _playerFireSpeed, 0, _playerFireDamage, 1);
         created->_x = this->_x + this->_size - 20;
         created->_y = this->_y + (this->_size)/2 - 20;
         return created;
