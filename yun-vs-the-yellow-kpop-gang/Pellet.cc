68e35d9bc005642436077704c332cf304347a88f
3678549 <lisa.hennebelle@upmc.fr>
2019-01-22 18:01:06 +0100

    help me

--- yun-vs-the-yellow-kpop-gang/Pellet.cc
+++ yun-vs-the-yellow-kpop-gang/Pellet.cc
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+
+
 Pellet::Pellet(float x, float y, float size, string imagePath,
                float speed, float direction, int damage, int target)
 :
@@ -14,4 +16,76 @@ Pellet::Pellet(float x, float y, float size, string imagePath,
     _target(target){}
 
 Pellet::~Pellet(){
+}
+
+void Pellet::nextFrame(){
+    switch(_frameNumber){
+        case 0 :
+            _frameNumber = 1;
+            if (this->_target == 0) this->_sprite.setTexture(_enemy_tex1);
+            else this->_sprite.setTexture(_ally_tex1);
+            break;
+        case 1 :
+            _frameNumber = 2;
+            if (this->_target == 0) this->_sprite.setTexture(_enemy_tex2);
+            else this->_sprite.setTexture(_ally_tex2);
+            break;
+        case 2 :
+            _frameNumber = 3;
+            if (this->_target == 0) this->_sprite.setTexture(_enemy_tex3);
+            else this->_sprite.setTexture(_ally_tex3);
+            break;
+        case 3 :
+            _frameNumber = 4;
+            if (this->_target == 0) this->_sprite.setTexture(_enemy_tex4);
+            else this->_sprite.setTexture(_ally_tex4);
+            break;
+        case 4 :
+            _frameNumber = 5;
+            if (this->_target == 0) this->_sprite.setTexture(_enemy_tex5);
+            else this->_sprite.setTexture(_ally_tex5);
+            break;
+        case 5 :
+            _frameNumber = 6;
+            if (this->_target == 0) this->_sprite.setTexture(_enemy_tex6);
+            else this->_sprite.setTexture(_ally_tex6);
+            break;
+        case 6 :
+            _frameNumber = 7;
+            if (this->_target == 0) this->_sprite.setTexture(_enemy_tex7);
+            else this->_sprite.setTexture(_ally_tex7);
+            break;
+        case 7 :
+            _frameNumber = 0;
+            if (this->_target == 0) this->_sprite.setTexture(_enemy_tex0);
+            else this->_sprite.setTexture(_ally_tex0);
+            break;
+
+        default:
+            break;
+    }
+}
+
+void Pellet::initializeTextures(){
+    int res = 0;
+    res += _ally_tex0.loadFromFile("images/_ally_spr_0");
+    res += _ally_tex1.loadFromFile("images/_ally_spr_1");
+    res += _ally_tex2.loadFromFile("images/_ally_spr_2");
+    res += _ally_tex3.loadFromFile("images/_ally_spr_3");
+    res += _ally_tex4.loadFromFile("images/_ally_spr_4");
+    res += _ally_tex5.loadFromFile("images/_ally_spr_5");
+    res += _ally_tex6.loadFromFile("images/_ally_spr_6");
+    res += _ally_tex7.loadFromFile("images/_ally_spr_7");
+    res += _enemy_tex0.loadFromFile("images/_enemy_spr_0");
+    res += _enemy_tex1.loadFromFile("images/_enemy_spr_1");
+    res += _enemy_tex2.loadFromFile("images/_enemy_spr_2");
+    res += _enemy_tex3.loadFromFile("images/_enemy_spr_3");
+    res += _enemy_tex4.loadFromFile("images/_enemy_spr_4");
+    res += _enemy_tex5.loadFromFile("images/_enemy_spr_5");
+    res += _enemy_tex6.loadFromFile("images/_enemy_spr_6");
+    res += _enemy_tex7.loadFromFile("images/_enemy_spr_7");
+    if (res){
+        cout << "Une texture de pellet n'a pas été chargée correctement." << endl;
+        exit(1);
+    }
 }
--- yun-vs-the-yellow-kpop-gang/Pellet.hh
+++ yun-vs-the-yellow-kpop-gang/Pellet.hh
@@ -7,10 +7,34 @@ class Pellet : public Entity {
                float speed, float direction, int damage, int target);
         ~Pellet();
 
+        void nextFrame();
+        void initializeTextures();
+
+        // Les textures de chaque sprite : static, la même pour toutes
+        // les pellets.
+        static sf::Texture _ally_tex0;
+        static sf::Texture _ally_tex1;
+        static sf::Texture _ally_tex2;
+        static sf::Texture _ally_tex3;
+        static sf::Texture _ally_tex4;
+        static sf::Texture _ally_tex5;
+        static sf::Texture _ally_tex6;
+        static sf::Texture _ally_tex7;
+        static sf::Texture _enemy_tex0;
+        static sf::Texture _enemy_tex1;
+        static sf::Texture _enemy_tex2;
+        static sf::Texture _enemy_tex3;
+        static sf::Texture _enemy_tex4;
+        static sf::Texture _enemy_tex5;
+        static sf::Texture _enemy_tex6;
+        static sf::Texture _enemy_tex7;
+
         float _speed;
         float _direction;
         int _damage;
         int _target; //0 pour joueur, 1 pour enemy
+        int _frameCounter;  // De 0 à 3
     private:
+        int _frameNumber;   // De 0 à 7
 
 };
