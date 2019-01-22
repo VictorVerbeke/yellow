1638955693c62bfdac6fedd61b6090a7a401250e
3678549 <lisa.hennebelle@upmc.fr>
2019-01-22 22:28:30 +0100

    Suppression de sprites inutiles, TODO: Modifier les créations de sprite.

--- yun-vs-the-yellow-kpop-gang/Pellet.cc
+++ yun-vs-the-yellow-kpop-gang/Pellet.cc
@@ -26,43 +26,43 @@ void Pellet::nextFrame(){
     switch(_frameNumber){
         case 0 :
             _frameNumber = 1;
-            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemy_tex1));
-            else this->_sprite.setTexture(*(Textures::_ally_tex1));
+            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemyPellet_tex1));
+            else this->_sprite.setTexture(*(Textures::_allyPellet_tex1));
             break;
         case 1 :
             _frameNumber = 2;
-            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemy_tex2));
-            else this->_sprite.setTexture(*(Textures::_ally_tex2));
+            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemyPellet_tex2));
+            else this->_sprite.setTexture(*(Textures::_allyPellet_tex2));
             break;
         case 2 :
             _frameNumber = 3;
-            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemy_tex3));
-            else this->_sprite.setTexture(*(Textures::_ally_tex3));
+            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemyPellet_tex3));
+            else this->_sprite.setTexture(*(Textures::_allyPellet_tex3));
             break;
         case 3 :
             _frameNumber = 4;
-            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemy_tex4));
-            else this->_sprite.setTexture(*(Textures::_ally_tex4));
+            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemyPellet_tex4));
+            else this->_sprite.setTexture(*(Textures::_allyPellet_tex4));
             break;
         case 4 :
             _frameNumber = 5;
-            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemy_tex5));
-            else this->_sprite.setTexture(*(Textures::_ally_tex5));
+            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemyPellet_tex5));
+            else this->_sprite.setTexture(*(Textures::_allyPellet_tex5));
             break;
         case 5 :
             _frameNumber = 6;
-            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemy_tex6));
-            else this->_sprite.setTexture(*(Textures::_ally_tex6));
+            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemyPellet_tex6));
+            else this->_sprite.setTexture(*(Textures::_allyPellet_tex6));
             break;
         case 6 :
             _frameNumber = 7;
-            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemy_tex7));
-            else this->_sprite.setTexture(*(Textures::_ally_tex7));
+            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemyPellet_tex7));
+            else this->_sprite.setTexture(*(Textures::_allyPellet_tex7));
             break;
         case 7 :
             _frameNumber = 0;
-            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemy_tex0));
-            else this->_sprite.setTexture(*(Textures::_ally_tex0));
+            if (this->_target == 0) this->_sprite.setTexture(*(Textures::_enemyPellet_tex0));
+            else this->_sprite.setTexture(*(Textures::_allyPellet_tex0));
             break;
 
         default:
--- yun-vs-the-yellow-kpop-gang/Textures.cc
+++ yun-vs-the-yellow-kpop-gang/Textures.cc
@@ -2,39 +2,77 @@
 
 Textures::Textures(){
 
-    // Initialisation :
-    _ally_tex0 = new sf::Texture();
-    _ally_tex1 = new sf::Texture();
-    _ally_tex2 = new sf::Texture();
-    _ally_tex3 = new sf::Texture();
-    _ally_tex4 = new sf::Texture();
-    _ally_tex5 = new sf::Texture();
-    _ally_tex6 = new sf::Texture();
-    _ally_tex7 = new sf::Texture();
-    _enemy_tex0 = new sf::Texture();
+    // Initialisation :;
+    _allyPellet_tex0 = new sf::Texture();
+    _allyPellet_tex1 = new sf::Texture();
+    _allyPellet_tex2 = new sf::Texture();
+    _allyPellet_tex3 = new sf::Texture();
+    _allyPellet_tex4 = new sf::Texture();
+    _allyPellet_tex5 = new sf::Texture();
+    _allyPellet_tex6 = new sf::Texture();
+    _allyPellet_tex7 = new sf::Texture();
+    _enemyPellet_tex0 = new sf::Texture();
+    _enemyPellet_tex1 = new sf::Texture();
+    _enemyPellet_tex2 = new sf::Texture();
+    _enemyPellet_tex3 = new sf::Texture();
+    _enemyPellet_tex4 = new sf::Texture();
+    _enemyPellet_tex5 = new sf::Texture();
+    _enemyPellet_tex6 = new sf::Texture();
+    _enemyPellet_tex7 = new sf::Texture();
     _enemy_tex1 = new sf::Texture();
     _enemy_tex2 = new sf::Texture();
     _enemy_tex3 = new sf::Texture();
-    _enemy_tex4 = new sf::Texture();
-    _enemy_tex5 = new sf::Texture();
-    _enemy_tex6 = new sf::Texture();
-    _enemy_tex7 = new sf::Texture();
+    _yun_still_tex = new sf::Texture();
+    _yun_hurt_tex = new sf::Texture();
+    _yun_hurt_f1_tex = new sf::Texture();
+    _panel_lvl1_tex = new sf::Texture();
+    _panel_lvl2_tex = new sf::Texture();
+    _panel_lvl3_tex = new sf::Texture();
+    _backgroung_ingame_tex = new sf::Texture();
+    _background_mainMenu_tex = new sf::Texture();
+    _background_options_tex = new sf::Texture();
+    _background_selectLvl_tex = new sf::Texture();
+    _cursor_tex = new sf::Texture();
 
-    _ally_tex0->loadFromFile("images/pellets/ally_spr_0.png");
-    _ally_tex1->loadFromFile("images/pellets/ally_spr_1.png");
-    _ally_tex2->loadFromFile("images/pellets/ally_spr_2.png");
-    _ally_tex3->loadFromFile("images/pellets/ally_spr_3.png");
-    _ally_tex4->loadFromFile("images/pellets/ally_spr_4.png");
-    _ally_tex5->loadFromFile("images/pellets/ally_spr_5.png");
-    _ally_tex6->loadFromFile("images/pellets/ally_spr_6.png");
-    _ally_tex7->loadFromFile("images/pellets/ally_spr_7.png");
-    _enemy_tex0->loadFromFile("images/pellets/enemy_spr_0.png");
-    _enemy_tex1->loadFromFile("images/pellets/enemy_spr_1.png");
-    _enemy_tex2->loadFromFile("images/pellets/enemy_spr_2.png");
-    _enemy_tex3->loadFromFile("images/pellets/enemy_spr_3.png");
-    _enemy_tex4->loadFromFile("images/pellets/enemy_spr_4.png");
-    _enemy_tex5->loadFromFile("images/pellets/enemy_spr_5.png");
-    _enemy_tex6->loadFromFile("images/pellets/enemy_spr_6.png");
-    _enemy_tex7->loadFromFile("images/pellets/enemy_spr_7.png");
-    
+    // Affectation
+    textureAffectation(_allyPellet_tex0, "images/pellets/ally_spr_0.png");
+    textureAffectation(_allyPellet_tex1, "images/pellets/ally_spr_1.png");
+    textureAffectation(_allyPellet_tex2, "images/pellets/ally_spr_2.png");
+    textureAffectation(_allyPellet_tex3, "images/pellets/ally_spr_3.png");
+    textureAffectation(_allyPellet_tex4, "images/pellets/ally_spr_4.png");
+    textureAffectation(_allyPellet_tex5, "images/pellets/ally_spr_5.png");
+    textureAffectation(_allyPellet_tex6, "images/pellets/ally_spr_6.png");
+    textureAffectation(_allyPellet_tex7, "images/pellets/ally_spr_7.png");
+    textureAffectation(_enemyPellet_tex0, "images/pellets/enemy_spr_0.png");
+    textureAffectation(_enemyPellet_tex1, "images/pellet""s/enemy_spr_1.png");
+    textureAffectation(_enemyPellet_tex2, "images/pellets/enemy_spr_2.png");
+    textureAffectation(_enemyPellet_tex3, "images/pellets/enemy_spr_3.png");
+    textureAffectation(_enemyPellet_tex4, "images/pellets/enemy_spr_4.png");
+    textureAffectation(_enemyPellet_tex5, "images/pellets/enemy_spr_5.png");
+    textureAffectation(_enemyPellet_tex6, "images/pellets/enemy_spr_6.png");
+    textureAffectation(_enemyPellet_tex7, "images/pellets/enemy_spr_7.png");
+    textureAffectation(_enemy_tex1, "images/idol1_32.png");
+    textureAffectation(_enemy_tex2, "images/idol2_32.png");
+    textureAffectation(_enemy_tex3, "images/idol3_32.png");
+    textureAffectation(_yun_still_tex, "images/yun_still_64.png");
+    textureAffectation(_yun_hurt_tex, "images/yun_hurt_64.png");
+    textureAffectation(_yun_hurt_f1_tex, "images/yun_hurt_f1_64.png");
+    textureAffectation(_panel_lvl1_tex, "images/Beenzino_panel_390_290.png");
+    textureAffectation(_panel_lvl2_tex, "images/GirlGeneration_panel_390_290.png");
+    textureAffectation(_panel_lvl3_tex, "images/Jonghyun_panel_390_290.png");
+    textureAffectation(_backgroung_ingame_tex, "images/ingame_bg.jpg");
+    textureAffectation(_background_mainMenu_tex, "images/main_menu_bg.png");
+    textureAffectation(_background_options_tex, "images/options_bg.png");
+    textureAffectation(_background_selectLvl_tex, "images/select_lvl_bg.png");
+    textureAffectation(_cursor_tex, "images/cursor_24.png");
+}
+
+void Textures::textureAffectation(sf::Texture* tex, string str){
+    int res = tex->loadFromFile(str);
+    if (!res) {
+        cout << "Error reading texture (\"" << str <<"\"), aborting." << endl;
+        exit(1);
+    } else {
+        cout << str << " chargé." << endl;
+    }
 }
